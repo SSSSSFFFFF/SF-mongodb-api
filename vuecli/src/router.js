e33f31f2d97486a4a822116534f3e499392b9053
S.F <1074260090@qq.com>
2019-06-25 09:56:20 +0800

    vue

--- vuecli/src/router.js
+++ vuecli/src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+
 
 Vue.use(Router)
 
@@ -14,14 +16,16 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
-      }
+      component: Login
+      // function () { 
+      //   return import( /* webpackChunkName: "login" */ './views/Login.vue')
+      // }
     }
   ]
 })
+console.log(Router)
